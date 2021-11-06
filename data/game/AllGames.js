81e5ed66b126e3855657249c13770502aa90e7a3
Ashun Kothari <ashun.kothari@soroco.com>
2021-11-06 22:32:44 +0530

    games added

--- data/game/AllGames.js
+++ data/game/AllGames.js
@@ -62,4 +62,109 @@ export const allGames = [
       "https://upload.wikimedia.org/wikipedia/en/thumb/2/21/FIFA_20_Standard_Edition_Cover.jpg/220px-FIFA_20_Standard_Edition_Cover.jpg",
     alt: "fifa-20",
   },
+  {
+    name: "Devil May Cry 4",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/DMC4COVER.jpg/220px-DMC4COVER.jpg",
+    alt: "devil-may-cry-4",
+  },
+  {
+    name: "DmC: Devil May Cry",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/7/79/DmC_box_art.png",
+    alt: "devil-may-cry-dmc",
+  },
+  {
+    name: "Far Cry 2",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Far_Cry_2_cover_art.jpg/220px-Far_Cry_2_cover_art.jpg",
+    alt: "far-cry-2",
+  },
+  {
+    name: "Far Cry 3",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Far_Cry_3_PAL_box_art.jpg/220px-Far_Cry_3_PAL_box_art.jpg",
+    alt: "far-cry-3 ",
+  },
+  {
+    name: "Far Cry 4",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Far_Cry_4_box_art.jpg/220px-Far_Cry_4_box_art.jpg",
+    alt: "fc4",
+  },
+  {
+    name: "Injustice 2",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/36/Injustice_2_Cover.jpg/220px-Injustice_2_Cover.jpg",
+    alt: "injustice2",
+  },
+  {
+    name: "Prototype",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/PROTOTYPE.png/220px-PROTOTYPE.png",
+    alt: "prototype",
+  },
+  {
+    name: "Prototype 2",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Prototype_2_game.jpg/220px-Prototype_2_game.jpg",
+    alt: "prototype2",
+  },
+  {
+    name: "GTA 5",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/a/a5/Grand_Theft_Auto_V.png/220px-Grand_Theft_Auto_V.png",
+    alt: "gta5",
+  },
+  {
+    name: "GTA: Vice City",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Vice-city-cover.jpg/220px-Vice-city-cover.jpg",
+    alt: "gta-vd",
+  },
+  {
+    name: "GTA: San Andreas",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/GTASABOX.jpg/220px-GTASABOX.jpg",
+    alt: "gta-sa",
+  },
+  {
+    name: "GTA: 4",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/b7/Grand_Theft_Auto_IV_cover.jpg/220px-Grand_Theft_Auto_IV_cover.jpg",
+    alt: "gta4",
+  },
+  {
+    name: "GTA: 3",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/GTA3boxcover.jpg/220px-GTA3boxcover.jpg",
+    alt: "gta3",
+  },
+  {
+    name: "CoD: Ghosts",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Call_of_duty_ghosts_box_art.jpg/220px-Call_of_duty_ghosts_box_art.jpg",
+    alt: "cod-ghosts",
+  },
+  {
+    name: "CoD: Advance Warfare",
+    type: "completed",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Advanced_Warfare.jpg/220px-Advanced_Warfare.jpg",
+    alt: "cod-aw",
+  },
 ];
